d6960bab09ee9a1a2ebed85c7117bd5c5ced6bb9
CppGoneWild <ludovicfalcot@gmail.com>
2019-12-11 04:36:19 +0100

    norme : add _ before private member

--- src/ux/Calandar.cpp
+++ src/ux/Calandar.cpp
@@ -13,30 +13,30 @@
 void ux::Calandar::display()
 {
 	std::tm tm = UniversalClock::to_tm(UniversalClock::now());
-	std::size_t size_copied = std::strftime(buffer_strftime, size_buffer_strftime, "%S:%M:%H %a %b %d-%m-%Y", &tm);
+	std::size_t size_copied = std::strftime(_buffer_strftime, _size_buffer_strftime, "%S:%M:%H %a %b %d-%m-%Y", &tm);
 
-	if (size_copied >= size_buffer_strftime || size_copied == 0) {
+	if (size_copied >= _size_buffer_strftime || size_copied == 0) {
 		COUT_ERROR << "std::strftime returned " << std::to_string(size_copied);
 		return ;
 	}
-	imgui_win_title = buffer_strftime;
-	imgui_win_title += "###Calandar";
+	_imgui_win_title = _buffer_strftime;
+	_imgui_win_title += "###Calandar";
 
-	if (ImGui::Begin(imgui_win_title.c_str())) {
-		ImGui::DragInt("Advance by : ", &time_incr);
-		if (ImGui::Button("sec")) UniversalClock::advance(UniversalClock::seconds(time_incr));
+	if (ImGui::Begin(_imgui_win_title.c_str())) {
+		ImGui::DragInt("Advance by : ", &_time_incr);
+		if (ImGui::Button("sec")) UniversalClock::advance(UniversalClock::seconds(_time_incr));
 		ImGui::SameLine();
-		if (ImGui::Button("min")) UniversalClock::advance(UniversalClock::minutes(time_incr));
+		if (ImGui::Button("min")) UniversalClock::advance(UniversalClock::minutes(_time_incr));
 		ImGui::SameLine();
-		if (ImGui::Button("hour")) UniversalClock::advance(UniversalClock::hours(time_incr));
+		if (ImGui::Button("hour")) UniversalClock::advance(UniversalClock::hours(_time_incr));
 		ImGui::SameLine();
-		if (ImGui::Button("day")) UniversalClock::advance(UniversalClock::days(time_incr));
+		if (ImGui::Button("day")) UniversalClock::advance(UniversalClock::days(_time_incr));
 		ImGui::SameLine();
-		if (ImGui::Button("week")) UniversalClock::advance(UniversalClock::weeks(time_incr));
+		if (ImGui::Button("week")) UniversalClock::advance(UniversalClock::weeks(_time_incr));
 		ImGui::SameLine();
-		if (ImGui::Button("month")) UniversalClock::advance(UniversalClock::months(time_incr));
+		if (ImGui::Button("month")) UniversalClock::advance(UniversalClock::months(_time_incr));
 		ImGui::SameLine();
-		if (ImGui::Button("year")) UniversalClock::advance(UniversalClock::years(time_incr));
+		if (ImGui::Button("year")) UniversalClock::advance(UniversalClock::years(_time_incr));
 	}
 	ImGui::End();
 }
--- src/ux/Calandar.hh
+++ src/ux/Calandar.hh
@@ -24,10 +24,10 @@ private:
 	Calandar & operator=(Calandar const &) = delete;
 	Calandar & operator=(Calandar &&)      = delete;
 
-	static constexpr std::size_t size_buffer_strftime = 128;
-	char buffer_strftime[size_buffer_strftime] = {0};
-	int time_incr = 1;
-	std::string imgui_win_title;
+	static constexpr std::size_t _size_buffer_strftime = 128;
+	char _buffer_strftime[_size_buffer_strftime] = {0};
+	int _time_incr = 1;
+	std::string _imgui_win_title;
 };
 
 
